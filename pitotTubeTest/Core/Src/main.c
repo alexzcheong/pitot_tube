a224db67cf9b278d3d9f0b11780b054c658d9aea
alexzcheong <113654786+alexzcheong@users.noreply.github.com>
2024-10-12 15:39:16 -0700

    Update main.c
    edited to match main branch for now

--- pitotTubeTest/Core/Src/main.c
+++ pitotTubeTest/Core/Src/main.c
@@ -1,30 +1,33 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
 #include <string.h>
 
-#define SENSOR_ADDRESS 0x25 << 1  // Sensor's I2C address (shifted for R/W)
+#define SENSOR_ADDRESS 0x25 << 1 // Sensor's I2C address (shifted for R/W)
 #define CMD_READ_PRODUCT_ID_1 0x36
 #define CMD_READ_PRODUCT_ID_2 0x7C
 #define CMD_READ_PRODUCT_ID_3 0xE1
 #define CMD_READ_PRODUCT_ID_4 0x02
+
+#define tempScaleFactor 200.0
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -56,8 +59,9 @@ UART_HandleTypeDef huart1;
 PCD_HandleTypeDef hpcd_USB_FS;
 
 /* Address for Pitot tube sensor */
-static const uint8_t pitotAddress = 0x25 << 1; //last bit reserved for R/W
-uint8_t calculateCRC(uint8_t *data, uint8_t length);
+static
+const uint8_t pitotAddress = 0x25 << 1; //last bit reserved for R/W
+uint8_t calculateCRC(uint8_t * data, uint8_t length);
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
@@ -84,93 +88,124 @@ static void MX_USART1_UART_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void) {
-    HAL_Init();
-    SystemClock_Config();
-    MX_GPIO_Init();
-    MX_I2C1_Init();
-    MX_USART1_UART_Init();
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_USART1_UART_Init();
 
-    uint8_t buf[9] = {0};
-    uint8_t cmd[] = {0x36, 0x1E};
-    uint8_t stop[] = {0x3F, 0xF9};
-    HAL_StatusTypeDef ret;
-    HAL_I2C_Master_Transmit(&hi2c1, SENSOR_ADDRESS, stop, sizeof(stop), HAL_MAX_DELAY);
-    HAL_Delay(20);
-    ret = HAL_I2C_Master_Transmit(&hi2c1, SENSOR_ADDRESS, cmd, sizeof(cmd), HAL_MAX_DELAY);
-    HAL_Delay(20);
-            if (ret != HAL_OK) {
-                char msg[] = "Failed to transmit I2C!\n\r";
-                HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-            }
-            else {
-                       // Receive product identifier data
-                       ret = HAL_I2C_Master_Receive(&hi2c1, SENSOR_ADDRESS, buf, sizeof(buf), HAL_MAX_DELAY);
-                       if (ret != HAL_OK) {
-                           char msg[] = "Failed to receive I2C!\n\r";
-                           HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-                       } else {
+  uint8_t buf[9] = {
+    0
+  };
+  uint8_t cmd[] = {
+    0x36,
+    0x1E
+  };
+  uint8_t stop[] = {
+    0x3F,
+    0xF9
+  };
 
-                           // Process and print product identifier
-                           char output[64];
-                           snprintf(output, sizeof(output), "Output: %02X%02X \t %02X%02X \t %02X%02X\n\r", buf[0], buf[1], buf[3], buf[4], buf[6], buf[7]);
-                           HAL_UART_Transmit(&huart1, (uint8_t*)output, strlen(output), HAL_MAX_DELAY);
-                       }
-                   }
-    while (1) {
-        // Transmit command to read product identifier
 
-    	// Receive product identifier data
-    	                       ret = HAL_I2C_Master_Receive(&hi2c1, SENSOR_ADDRESS, buf, sizeof(buf), HAL_MAX_DELAY);
-    	                       if (ret != HAL_OK) {
-    	                           char msg[] = "Failed to receive I2C!\n\r";
-    	                           HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-    	                       } else {
 
-    	                           // Process and print product identifier
-    	                           char output[64];
-    	                           snprintf(output, sizeof(output), "Output: %02X%02X \t %02X%02X \t %02X%02X\n\r", buf[0], buf[1], buf[3], buf[4],buf[6], buf[7]);
-    	                           HAL_UART_Transmit(&huart1, (uint8_t*)output, strlen(output), HAL_MAX_DELAY);
-    	                       }
-        // Delay between attempts
-        HAL_Delay(1); // 1 ms delay
+  /* variable for temperature, scaleFactor, and diffPressure*/
+  uint16_t tempUnScaled;
+  int16_t diffPressureUnScaled;
+  uint16_t scaleFactorDP;
+  uint16_t scaledTemp;
+  uint16_t scaledDP;
+
+  HAL_StatusTypeDef ret;
+  HAL_I2C_Master_Transmit( & hi2c1, SENSOR_ADDRESS, stop, sizeof(stop), HAL_MAX_DELAY);
+  HAL_Delay(20);
+  ret = HAL_I2C_Master_Transmit( & hi2c1, SENSOR_ADDRESS, cmd, sizeof(cmd), HAL_MAX_DELAY);
+  HAL_Delay(20);
+  if (ret != HAL_OK) {
+    char msg[] = "Failed to transmit I2C!\n\r";
+    HAL_UART_Transmit( & huart1, (uint8_t * ) msg, strlen(msg), HAL_MAX_DELAY);
+  } else {
+    // Receive product identifier data
+    ret = HAL_I2C_Master_Receive( & hi2c1, SENSOR_ADDRESS, buf, sizeof(buf), HAL_MAX_DELAY);
+    if (ret != HAL_OK) {
+      char msg[] = "Failed to receive I2C!\n\r";
+      HAL_UART_Transmit( & huart1, (uint8_t * ) msg, strlen(msg), HAL_MAX_DELAY);
     }
-}
-uint8_t calculateCRC(uint8_t *data, uint8_t length) {
-    uint8_t crc = 0xFF;
-    uint8_t polynomial = 0x31;  // CRC-8 polynomial
+  }
+  while (1) {
+    // Transmit command to read product identifier
 
-    for (uint8_t i = 0; i < length; i++) {
-        crc ^= data[i];
-        for (uint8_t bit = 8; bit > 0; --bit) {
-            if (crc & 0x80) {
-                crc = (crc << 1) ^ polynomial;
-            } else {
-                crc = (crc << 1);
-            }
-        }
+    // Receive product identifier data
+    ret = HAL_I2C_Master_Receive( & hi2c1, SENSOR_ADDRESS, buf, sizeof(buf), HAL_MAX_DELAY);
+    if (ret != HAL_OK) {
+      char msg[] = "Failed to receive I2C!\n\r";
+      HAL_UART_Transmit( & huart1, (uint8_t * ) msg, strlen(msg), HAL_MAX_DELAY);
+    } else {
+      /* populate temp buf[3] is msb of temp, buf[4] is lsb of temp*/
+      tempUnScaled = ((buf[3] << 8) | buf[4]);
+      scaledTemp = (int)tempUnScaled / tempScaleFactor;
+
+      /* populate differential pressure, buf[0] is dp msb, buf[1] is db lsb */
+      diffPressureUnScaled = (buf[0] << 8) | buf[1];
+
+      /* populate differetial pressure scale factor buf[6] is msb, buf[7] is lsb (of scale factor)*/
+      scaleFactorDP = (buf[6] << 8) | buf[7];
+
+
+      /* pressure is equal to diffPressureUnScaled / scaleFactorDP */
+      scaledDP =  diffPressureUnScaled /scaleFactorDP;
+
+      // Process and print product identifier
+      char output[64];
+      //snprintf(output, sizeof(output), "Differential Pressure %d, Temperature: %d\n", scaledDP, scaledTemp);
+      HAL_UART_Transmit( & huart1, (uint8_t * ) output, strlen(output), HAL_MAX_DELAY);
+
+      snprintf(output, sizeof(output), "Differential unscaled %d, scalefactor: %d\n", diffPressureUnScaled, scaleFactorDP);
+      HAL_UART_Transmit( & huart1, (uint8_t * ) output, strlen(output), HAL_MAX_DELAY);
     }
-    return crc;
+    // Delay between attempts
+    HAL_Delay(100); // 1 ms delay
+  }
 }
+uint8_t calculateCRC(uint8_t * data, uint8_t length) {
+  uint8_t crc = 0xFF;
+  uint8_t polynomial = 0x31; // CRC-8 polynomial
 
+  for (uint8_t i = 0; i < length; i++) {
+    crc ^= data[i];
+    for (uint8_t bit = 8; bit > 0; --bit) {
+      if (crc & 0x80) {
+        crc = (crc << 1) ^ polynomial;
+      } else {
+        crc = (crc << 1);
+      }
+    }
+  }
+  return crc;
+}
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {
+    0
+  };
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {
+    0
+  };
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {
+    0
+  };
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -178,42 +213,38 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig( & RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+    RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig( & RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB|RCC_PERIPHCLK_USART1
-                              |RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_USART1 |
+    RCC_PERIPHCLK_I2C1;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
   PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
   PeriphClkInit.USBClockSelection = RCC_USBCLKSOURCE_PLL;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
+  if (HAL_RCCEx_PeriphCLKConfig( & PeriphClkInit) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_I2C1_Init(void) {
 
   /* USER CODE BEGIN I2C1_Init 0 */
 
@@ -231,22 +262,19 @@ static void MX_I2C1_Init(void)
   hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
   hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
   hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
+  if (HAL_I2C_Init( & hi2c1) != HAL_OK) {
     Error_Handler();
   }
 
   /** Configure Analogue filter
-  */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
-  {
+   */
+  if (HAL_I2CEx_ConfigAnalogFilter( & hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK) {
     Error_Handler();
   }
 
   /** Configure Digital filter
-  */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
-  {
+   */
+  if (HAL_I2CEx_ConfigDigitalFilter( & hi2c1, 0) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN I2C1_Init 2 */
@@ -256,12 +284,11 @@ static void MX_I2C1_Init(void)
 }
 
 /**
-  * @brief SPI1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI1_Init(void)
-{
+ * @brief SPI1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SPI1_Init(void) {
 
   /* USER CODE BEGIN SPI1_Init 0 */
 
@@ -285,8 +312,7 @@ static void MX_SPI1_Init(void)
   hspi1.Init.CRCPolynomial = 7;
   hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
   hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-  if (HAL_SPI_Init(&hspi1) != HAL_OK)
-  {
+  if (HAL_SPI_Init( & hspi1) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN SPI1_Init 2 */
@@ -296,12 +322,11 @@ static void MX_SPI1_Init(void)
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART1_UART_Init(void)
-{
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
 
   /* USER CODE BEGIN USART1_Init 0 */
 
@@ -320,8 +345,7 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
   huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
+  if (HAL_UART_Init( & huart1) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN USART1_Init 2 */
@@ -331,12 +355,11 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
-  * @brief USB Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USB_PCD_Init(void)
-{
+ * @brief USB Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USB_PCD_Init(void) {
 
   /* USER CODE BEGIN USB_Init 0 */
 
@@ -351,8 +374,7 @@ static void MX_USB_PCD_Init(void)
   hpcd_USB_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
   hpcd_USB_FS.Init.low_power_enable = DISABLE;
   hpcd_USB_FS.Init.battery_charging_enable = DISABLE;
-  if (HAL_PCD_Init(&hpcd_USB_FS) != HAL_OK)
-  {
+  if (HAL_PCD_Init( & hpcd_USB_FS) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN USB_Init 2 */
@@ -362,15 +384,16 @@ static void MX_USB_PCD_Init(void)
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+  GPIO_InitTypeDef GPIO_InitStruct = {
+    0
+  };
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
@@ -380,37 +403,37 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOE, CS_I2C_SPI_Pin|LD4_Pin|LD3_Pin|LD5_Pin
-                          |LD7_Pin|LD9_Pin|LD10_Pin|LD8_Pin
-                          |LD6_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, CS_I2C_SPI_Pin | LD4_Pin | LD3_Pin | LD5_Pin |
+    LD7_Pin | LD9_Pin | LD10_Pin | LD8_Pin |
+    LD6_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : DRDY_Pin MEMS_INT3_Pin MEMS_INT4_Pin MEMS_INT1_Pin
                            MEMS_INT2_Pin */
-  GPIO_InitStruct.Pin = DRDY_Pin|MEMS_INT3_Pin|MEMS_INT4_Pin|MEMS_INT1_Pin
-                          |MEMS_INT2_Pin;
+  GPIO_InitStruct.Pin = DRDY_Pin | MEMS_INT3_Pin | MEMS_INT4_Pin | MEMS_INT1_Pin |
+    MEMS_INT2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOE, & GPIO_InitStruct);
 
   /*Configure GPIO pins : CS_I2C_SPI_Pin LD4_Pin LD3_Pin LD5_Pin
                            LD7_Pin LD9_Pin LD10_Pin LD8_Pin
                            LD6_Pin */
-  GPIO_InitStruct.Pin = CS_I2C_SPI_Pin|LD4_Pin|LD3_Pin|LD5_Pin
-                          |LD7_Pin|LD9_Pin|LD10_Pin|LD8_Pin
-                          |LD6_Pin;
+  GPIO_InitStruct.Pin = CS_I2C_SPI_Pin | LD4_Pin | LD3_Pin | LD5_Pin |
+    LD7_Pin | LD9_Pin | LD10_Pin | LD8_Pin |
+    LD6_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOE, & GPIO_InitStruct);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(B1_GPIO_Port, & GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -418,30 +441,26 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
-  }
+  while (1) {}
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t * file, uint32_t line) {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
